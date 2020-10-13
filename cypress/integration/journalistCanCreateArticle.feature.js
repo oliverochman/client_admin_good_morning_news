673ef760905d95ecdf15537f608ad7ba73f68b3e
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-10-13 17:09:17 +0200

    WIP. Starting on first happy path test.

---
+++ cypress/integration/journalistCanCreateArticle.feature.js
@@ -0,0 +1,23 @@
+describe("Journalist can create article", () => {
+  beforeEach(() => {
+    cy.login()
+  })
+
+  context('succesfully', () => {
+    beforeEach(()=> {
+      cy.route({
+        method: "POST",
+        url: "http://localhost:3000/api/v1/articles*",
+        response: "fixture:articles_create_success.json"
+      });
+    });
+
+    it('with title and content', () => {
+      cy.get("[data-cy='title']").type("An absolutely article fantastic")
+      cy.get("[data-cy='content']").type("Article content")
+      cy.get("")
+    })
+  });
+
+
+})
