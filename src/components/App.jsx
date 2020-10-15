6ab9e484cafaf60b3d303b8752cabdea74d48f9a
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-10-15 13:56:02 +0200

    Creating component Authenticate and Home, installing react router dom

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Authenticate from "./Authenticate";
+import Home from "./Home";
+
+const App = () => {
+  const [authenticated, setAuthenticated] = useState(false);
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          {authenticated ? <Redirect to="/Home" /> : <Authenticate/>}
+        </Route>
+        <Route exact path="/Home">
+          {authenticated ? <Home /> : <Redirect to="/" />}
+        </Route>
+      </Switch>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Authenticate.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Authenticate = () => {
+  return (
+    <div>
+      <p>Authenticate</p>
+    </div>
+  )
+}
+
+export default Authenticate
